0f8c08465451132c652f5c00dad0c6d553994dac
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-05 12:21:40 -0800

    basic

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hi there 2!</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
