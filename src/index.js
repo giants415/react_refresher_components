0cc8e048bfb04f1bb65161aabe8993c63361069d
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-05 18:50:14 -0800

    comment detail as child to approval card

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Michael" timeAgo="Today at 4:30pm" commentText="IDK seems fishy" avatar={faker.image.avatar()} />
-      <CommentDetail author="Jimmy" timeAgo="Today at 2:30pm" commentText="Insightful post" avatar={faker.image.avatar()} />
-      <CommentDetail author="Samantha" timeAgo="Yesterday at 1:20pm" commentText="Cool blog post" avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <CommentDetail author="Michael" timeAgo="Today at 4:30pm" commentText="IDK seems fishy" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Jimmy" timeAgo="Today at 2:30pm" commentText="Insightful post" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Samantha" timeAgo="Yesterday at 1:20pm" commentText="Cool blog post" avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 };
